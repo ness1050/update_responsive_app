0bb32257be1cee91643f6a246e74a991d759456a
Naseem Qurbanli <nq222af@student.lnu.se>
2025-05-29 16:30:40 +0200

    Added arrow keyfeatures

--- public/script.js
+++ public/script.js
@@ -11,3 +11,36 @@ document.addEventListener("DOMContentLoaded", () => {
     };
     dateElement.textContent = currentDate.toLocaleDateString(undefined, opition);
 });
+
+const navLinks = document.querySelectorAll('.nav_items a');
+const footlinks = document.querySelectorAll('.footer a');
+let current_index = 0;
+let footer_index =0;
+
+document.addEventListener('keydown', (e) => {
+    const active = document.activeElement;
+
+    if ([...navLinks].includes(active)) {
+        if (e.key === 'ArrowRight') {
+        current_index = (current_index + 1) % navLinks.length;
+        navLinks[current_index].focus();
+        e.preventDefault();
+        } else if (e.key === 'ArrowLeft') {
+            current_index = (current_index - 1 + navLinks.length) % navLinks.length;
+            navLinks[current_index].focus();
+            e.preventDefault();
+        }
+    }
+    if ([...footlinks].includes(active)) {
+        if (e.key === 'ArrowRight') {
+        footer_index = (footer_index + 1) % footlinks.length;
+        footlinks[footer_index].focus();
+        e.preventDefault();
+        } else if (e.key === 'ArrowLeft') {
+        footer_index = (footer_index - 1 + footlinks.length) % footlinks.length;
+        footlinks[footer_index].focus();
+         e.preventDefault();
+        }
+       
+    }
+});
